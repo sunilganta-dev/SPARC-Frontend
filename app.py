0b8846c8b96f7a2680c1664be7392c816bbff2bd
Yair Chaya <yair@entrancegrpe.com>
2025-08-27 13:36:50 -0400

    Update API URL in environment file and change app port to 5006

--- app.py
+++ app.py
@@ -74,4 +74,4 @@ def index():
     return render_template('index.html', stats=stats)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(debug=True, host='0.0.0.0', port=5006)
